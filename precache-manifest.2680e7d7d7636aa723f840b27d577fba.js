81013e92f501e4e84222276f4e5a2fc8fffdc33f
talha619 <hafiz.talhakh@gmail.com>
2020-05-14 16:53:15 +0500

    deploying website on github pages

---
+++ precache-manifest.2680e7d7d7636aa723f840b27d577fba.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed29a4518763e0adbe69de096fe85f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b5d176c0fcec7057542",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9ae122668eac35af2f24",
+    "url": "/static/css/main.81713726.chunk.css"
+  },
+  {
+    "revision": "1b5d176c0fcec7057542",
+    "url": "/static/js/2.2bd6d1f3.chunk.js"
+  },
+  {
+    "revision": "584e7a16bcc0805d270921d9a679d2f4",
+    "url": "/static/js/2.2bd6d1f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae122668eac35af2f24",
+    "url": "/static/js/main.37a59d61.chunk.js"
+  },
+  {
+    "revision": "10637b4b3801bc260045",
+    "url": "/static/js/runtime-main.0be6d443.js"
+  },
+  {
+    "revision": "cb062359bbab0df88edb0e2f42e2c7d8",
+    "url": "/static/media/Batool-dp.cb062359.jpg"
+  },
+  {
+    "revision": "ad6922a63e9287bd3da2601abd717e1a",
+    "url": "/static/media/SohaibNew.ad6922a6.jpeg"
+  },
+  {
+    "revision": "521bdcca001777cc46801bc8759890e1",
+    "url": "/static/media/Talha-dp-new.521bdcca.jpg"
+  },
+  {
+    "revision": "cf25501f645702aa97dd4d4f7ebc13db",
+    "url": "/static/media/YEO 1.cf25501f.PNG"
+  },
+  {
+    "revision": "52bec8f6a5fb989a25429cd17b52c036",
+    "url": "/static/media/about-us.52bec8f6.png"
+  },
+  {
+    "revision": "ddc2dbf77f665d35e091d2cff37ac3e9",
+    "url": "/static/media/banner4.ddc2dbf7.jpg"
+  },
+  {
+    "revision": "b78617e158499959634e359108f38067",
+    "url": "/static/media/giverThumbnail.b78617e1.png"
+  },
+  {
+    "revision": "a58ac9046fdf867aabc7500d83ae1ff1",
+    "url": "/static/media/giverpaysfirst.a58ac904.PNG"
+  },
+  {
+    "revision": "110272caf82625aca861cb1b4625c0d5",
+    "url": "/static/media/heights.110272ca.jpg"
+  },
+  {
+    "revision": "1fa647fd393b2151ee0718395b594d61",
+    "url": "/static/media/logo.1fa647fd.png"
+  },
+  {
+    "revision": "457dc95db551e67a59372eaa583002ae",
+    "url": "/static/media/mylogo-new.457dc95d.png"
+  },
+  {
+    "revision": "6b6a077a33d8028ec6c5f18e848035ef",
+    "url": "/static/media/owais-1.6b6a077a.jpg"
+  },
+  {
+    "revision": "7fb6d5de4000ab719bb642cf5bdb19e0",
+    "url": "/static/media/shaheerenterprises.7fb6d5de.PNG"
+  },
+  {
+    "revision": "6666ab0fc21712b72134dd15f5d26a15",
+    "url": "/static/media/shaheerenterprises1.6666ab0f.PNG"
+  },
+  {
+    "revision": "b69e958fbfcbeb398fba17001b9d52ba",
+    "url": "/static/media/strange.b69e958f.jpg"
+  },
+  {
+    "revision": "32590a115ec8c61e2b0e35976f0ce300",
+    "url": "/static/media/subscribe-bg.32590a11.png"
+  },
+  {
+    "revision": "be659aeca3182e574667417330324ab9",
+    "url": "/static/media/sunflower.be659aec.PNG"
+  },
+  {
+    "revision": "7de648843a074ad14938fa022ab0ee32",
+    "url": "/static/media/sunflowerThumbnail.7de64884.PNG"
+  },
+  {
+    "revision": "42aae2f5868820a64c019e1e3ec36d6d",
+    "url": "/static/media/switcheroo.42aae2f5.PNG"
+  },
+  {
+    "revision": "c427b59ef7da3c4fcb057cf2d25db1a3",
+    "url": "/static/media/switcherooThumb1.c427b59e.png"
+  },
+  {
+    "revision": "f33a2dcc8bbb654c5791f581dab8f59a",
+    "url": "/static/media/testimonial.f33a2dcc.jpg"
+  },
+  {
+    "revision": "5b9c135f2542fe4ec7755fa524ddba19",
+    "url": "/static/media/vibrantWeb.5b9c135f.PNG"
+  },
+  {
+    "revision": "734ceafce9a90dd70073ac8d50d38f72",
+    "url": "/static/media/yeo1.734ceafc.PNG"
+  }
+]);
